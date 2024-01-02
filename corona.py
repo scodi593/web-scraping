a1f0b0e814dc1fab2babc4365c7a00dfb5ca5bb5
SATHVIKA AMBEKAR <98017772+scodi593@users.noreply.github.com>
2024-01-02 10:03:09 +0530

    corona.py

---
+++ corona.py
@@ -0,0 +1,46 @@
+# Import required modules
+import requests
+import bs4
+import pandas as pd
+
+
+
+# Make requests from webpage
+url = 'https://www.worldometers.info/coronavirus/country/india/'
+result = requests.get(url)
+
+
+
+# Creating soap object
+soup = bs4.BeautifulSoup(result.text,'lxml')
+
+
+
+# Searching div tags having maincounter-number class
+cases = soup.find_all('div' ,class_= 'maincounter-number')
+
+
+
+# List to store number of cases
+data = []
+
+# Find the span and get data from it
+for i in cases:
+	span = i.find('span')
+	data.append(span.string)
+
+# Display number of cases
+print(data)
+
+
+
+# Creating dataframe
+df = pd.DataFrame({"CoronaData": data})
+
+# Naming the columns
+df.index = ['TotalCases', ' Deaths', 'Recovered']
+
+
+
+# Exporting data into Excel
+df.to_csv('Corona_Data.csv')
